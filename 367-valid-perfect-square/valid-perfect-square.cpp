b3e35852a0b42a7e2ea4f22177518c68fb41182f
BilalSaleem03 <155255932+BilalSaleem03@users.noreply.github.com>
2024-03-15 21:13:09 +0500

    Time: 0 ms (100.00%) | Memory: 7.2 MB (13.61%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+       if (num < 1)
+        return false;
+
+        int i = 1;
+        while (num > 0) 
+        {
+            num -= i;
+            i += 2;
+        }
+        if(num == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
